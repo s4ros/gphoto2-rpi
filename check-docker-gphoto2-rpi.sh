b302871de76811ae8aee9a968c3c010d44caf348
s4ros <s4ros@infinigy.pl>
2017-06-02 14:56:41 +0200

    parametrized check-docker-gphoto2-rpi.sh server-side script
    - added default values for crucial variables
    - now it's easy to adjust script by passing proper arguments into the script

--- check-docker-gphoto2-rpi.sh
+++ check-docker-gphoto2-rpi.sh
@@ -4,25 +4,27 @@
 ## Simple "watchdog" script which can be put into cron
 ## to ensure that container is up and running
 ##############################################################################
+# path to directory where to mount docker volume
+RPI_PATH=${1:-/opt/home/s4ros/gphoto2-rpi}
+SERVER=${2:-localhost:56789}
 
-
-function reset_nikon_container()
+function reset-gphoto2-container()
 {
   docker stop gphoto2-rpi || docker kill gphoto2-rpi
   docker rm gphoto2-rpi
-  docker run -d --name gphoto2-rpi -v /opt/home/s4ros/gphoto2-rpi:/rpi -v /etc/localtime:/etc/localtime:ro -p 56789:56789 s4ros/gphoto2-rpi
+  docker run -d --name gphoto2-rpi -v ${RPI_PATH}:/rpi -v /etc/localtime:/etc/localtime:ro -p 127.0.0.1:56789:56789 s4ros/gphoto2-rpi
 }
 
 if [[ "$1" == "restart" ]]; then
-  reset_nikon_container
+  reset-gphoto2-container
   exit 0
 fi
 
 if [[ $(docker ps | grep 56789 | wc -l) -ne 1 ]]; then
-  reset_nikon_container
+  reset-gphoto2-container
 fi
 
-curl -s --connect-timeout 15 http://infinigy.pl:56789/ &> /dev/null
+curl -s --connect-timeout 15 http://${SERVER}/ &> /dev/null
 if [[ $? -ne 0 ]]; then
-  reset_nikon_container
+  reset-gphoto2-container
 fi
